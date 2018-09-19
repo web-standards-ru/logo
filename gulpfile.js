6f56e8e4025dd1b5577f4bcf84535f7210ecdfea
Vadim Makeev <pepelsbey@gmail.com>
2018-09-19 16:09:01 +0300

    Рефакторит, добавляет графику

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp'),
-	svgmin = require('gulp-svgmin');
-
-gulp.task('default', function () {
-	return gulp.src('src/**/*.svg')
-		.pipe(svgmin())
-		.pipe(gulp.dest('dest'));
-});
